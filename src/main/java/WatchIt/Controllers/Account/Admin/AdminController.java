9f306470570086ec1c083b934657560ea1b45718
Medo101 <107288702+Medo101@users.noreply.github.com>
2024-12-10 03:39:58 +0200

    incomplete admin features

--- src/main/java/WatchIt/Controllers/Account/Admin/AdminController.java
+++ src/main/java/WatchIt/Controllers/Account/Admin/AdminController.java
@@ -1,4 +1,5 @@
 package WatchIt.Controllers.Account.Admin;
-
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
 public class AdminController {
 }
--- src/main/java/WatchIt/Views/AdminView.java
+++ src/main/java/WatchIt/Views/AdminView.java
@@ -1,4 +1,30 @@
 package WatchIt.Views;
 
+import WatchIt.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+
+import java.io.IOException;
+
 public class AdminView {
+    public static FXMLLoader AdminMenuControllerScene() {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(Application.class.getResource("/Fxml/Account/Admin/Controller.fxml"));
+        return fxmlLoader;
+    }
+    public static FXMLLoader MovieControllerScene() {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(Application.class.getResource("/Fxml/Account/Admin/MoviesController.fxml"));
+        return fxmlLoader;
+    }
+    public static Scene getScene(FXMLLoader fxmlLoader){
+        try{
+            Scene scene = new Scene(fxmlLoader.load());
+            return scene;
+        }catch (IOException e){
+            System.out.println("Error: While Getting Scene");
+            System.out.println(e);
+        }
+        return null;
+    }
 }
