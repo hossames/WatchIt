0f8856d0d472952f3a57e7ec2a130097f2529633
hussein145 <140104749+hussein145@users.noreply.github.com>
2024-12-14 14:49:07 +0200

    Update LoginController.java

--- src/main/java/WatchIt/Controllers/Enter/LoginController.java
+++ src/main/java/WatchIt/Controllers/Enter/LoginController.java
@@ -1,24 +1,26 @@
 package WatchIt.Controllers.Enter;
 
 import WatchIt.Models.Model;
-import WatchIt.Views.AdminView;
 import WatchIt.Views.ClientView;
 import WatchIt.Views.MainView;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
-import src.AccountControl.Admin;
-import src.ContentControl.Content;
 import src.DataBase.DataBase;
-import src.DataBase.DataObjectController;
-
 import java.io.IOException;
 public class LoginController {
     // Fx Entities
     public BorderPane LoginPage;
     public TextField Email;
     public PasswordField Password;
+    public Button LoginButton;
+    public Hyperlink Register;
+    public Hyperlink Forget;
 
     //Variables
     double mousePressedX,mousePressedY;
@@ -30,6 +32,22 @@ public class LoginController {
         }catch (IOException e){
             e.printStackTrace();
         }
+        LoginPage.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                mousePressedX = mouseEvent.getX();
+                mousePressedY = mouseEvent.getY();
+            }
+        });
+        LoginPage.addEventFilter(MouseEvent.MOUSE_DRAGGED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                double crrX = mouseEvent.getScreenX();
+                double crrY = mouseEvent.getScreenY();
+                Model.getInstance().getViewFactory().PrimaryStage.setX(crrX - mousePressedX);
+                Model.getInstance().getViewFactory().PrimaryStage.setY(crrY - mousePressedY);
+            }
+        });
     }
 
     //Enter transitions
@@ -44,7 +62,7 @@ public class LoginController {
     public void Login(){
         if(DataBase.getInstance().Login(Email.getText(),Password.getText())) {
             System.out.println("Login Successful");
-
+            Model.getInstance().getViewFactory().Show(ClientView.ClientMenuControllerScene());
         }
         else{
             Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -57,12 +75,13 @@ public class LoginController {
 
     //Register Page
     public void Register(){
-        Model.getInstance().getViewFactory().Show(MainView.RegisterScene());
+
     }
 
     //Forget PasswordPage
     public void Forget(){
-        Model.getInstance().getViewFactory().Show(MainView.FavoriteNameScene(Email.getText()));
+        CheckFavoriteNameController.Email = Email.getText();
+        Model.getInstance().getViewFactory().Show(MainView.FavoriteNameScene());
     }
 }
 
