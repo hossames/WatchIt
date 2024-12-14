5785cbeab60f02b531b23da6cef279ff886b632c
hussein145 <140104749+hussein145@users.noreply.github.com>
2024-12-14 14:48:42 +0200

    Update ContentCardController.java

--- src/main/java/WatchIt/Controllers/Content/ContentCardController.java
+++ src/main/java/WatchIt/Controllers/Content/ContentCardController.java
@@ -1,4 +1,35 @@
 package WatchIt.Controllers.Content;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+
+import src.ContentControl.Movie;
+import src.DataBase.DataBase;
+import src.DataBase.DataObject;
+
+
+
 public class ContentCardController {
+
+    @FXML
+    ImageView poster;
+
+
+    @FXML
+    private Label ContentName;
+
+    DataObject content;
+
+    public ContentCardController(DataObject content) {
+        this.content = content;
+
+    }
+
+    public void initialize() {
+        ContentName.setText(content.getName(2));
+        poster.setImage(content.getImage());
+    }
 }
