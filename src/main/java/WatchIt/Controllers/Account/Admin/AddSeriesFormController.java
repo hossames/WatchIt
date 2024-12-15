1fc62bf5c18997819a1027a60017dc209ed32776
Ali-Hady <68974591+Ali-Hady@users.noreply.github.com>
2024-12-15 16:23:58 +0200

    ali is here boys

--- src/main/java/WatchIt/Controllers/Account/Admin/AddSeriesFormController.java
+++ src/main/java/WatchIt/Controllers/Account/Admin/AddSeriesFormController.java
@@ -1,9 +1,71 @@
 package WatchIt.Controllers.Account.Admin;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 public class AddSeriesFormController {
 
+    @FXML
+    private Button add_btn;
+
+    @FXML
+    private Button add_cast_member;
+
+    @FXML
+    private Button add_genre;
+
+    @FXML
+    private TextField budget;
+
+    @FXML
+    private ChoiceBox<?> castMembers;
+
+    @FXML
+    private TextField country;
+
+    @FXML
+    private TextField director;
+
+    @FXML
+    private TextField duration;
+
+    @FXML
+    private DatePicker first_date;
+
+    @FXML
+    private ChoiceBox<?> genres;
+
+    @FXML
+    private TextField id;
+
+    @FXML
+    private CheckBox is_going;
+
+    @FXML
+    private DatePicker last_date;
+
+    @FXML
+    private TextField num_of_episodes;
+
+    @FXML
+    private TextField num_of_seasons;
+
+    @FXML
+    private TextField revenue;
+
+    @FXML
+    void addCastMember(MouseEvent event) {
+
+    }
+
+    @FXML
+    void addGenre(MouseEvent event) {
+
+    }
 
 }
